57af801aa096cc8205ad4f529c668e4784262cd8
Philipp Schwarzmüller <philipp.schwarzmueller@gmail.com>
2023-01-04 11:08:46 +0100

    implement file read

--- fdf.h
+++ fdf.h
@@ -6,9 +6,10 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 15:44:00 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/12/30 15:46:04 by pschwarz         ###   ########.fr       */
+/*   Updated: 2023/01/04 10:33:43 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./lib/libft/libft.h"
 #include "./lib/mlx42/include/MLX42/MLX42.h"
+#include <fcntl.h>
--- src/fdf.c
+++ src/fdf.c
@@ -6,13 +6,47 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 15:08:16 by pschwarz          #+#    #+#             */
-/*   Updated: 2022/12/30 15:46:52 by pschwarz         ###   ########.fr       */
+/*   Updated: 2023/01/04 11:07:34 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../fdf.h"
 
-int	main(void)
+char	**read_file(char *path);
+
+int	main(int argc, char **argv)
 {
-	ft_printf("HELLO WORLD");
+	char	**test;
+	int		i;
+
+	if (argc != 2)
+		return (0);
+	i = 0;
+	test = read_file(argv[argc - 1]);
+	while (test[i] != NULL)
+	{
+		ft_printf("%s", test[i]);
+		i++;
+	}
 	return (0);
+}
+
+char	**read_file(char *path)
+{
+	char	**contents;
+	int		fd;
+	char	*curr_str;
+	int		i;
+
+	contents = malloc(sizeof(char *) * 20);
+	fd = open(path, O_RDONLY);
+	curr_str = " ";
+	i = 0;
+	while (curr_str != NULL)
+	{
+		curr_str = get_next_line(fd);
+		contents[i] = curr_str;
+		i++;
+	}
+	contents[i] = NULL;
+	return (contents);
 }
