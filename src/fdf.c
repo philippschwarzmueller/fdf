705e8b92c23be760a707f241cac72ce69e64808d
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-01-10 13:13:05 +0100

    fix imports and boilerplate setup

--- src/fdf.c
+++ src/fdf.c
@@ -6,20 +6,18 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 15:08:16 by pschwarz          #+#    #+#             */
-/*   Updated: 2023/01/04 15:20:18 by pschwarz         ###   ########.fr       */
+/*   Updated: 2023/01/10 13:11:13 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "../fdf.h"
 
 char	**read_file(char *path);
-void	draw_content(mlx_image_t image, char **content);
 
 int	main(int argc, char **argv)
 {
 	char		**test;
 	int			i;
-	mlx_t		*window;
-	mlx_image_t	*test_image;
 
 	if (argc != 2)
 		return (0);
@@ -30,36 +28,7 @@ int	main(int argc, char **argv)
 		ft_printf("%s", test[i]);
 		i++;
 	}
-	window = mlx_init(WIDTH, HEIGHT, "FdF", true);
-	if (!window)
-		ft_printf("failed to draw window");
-	test_image = mlx_new_image(window, WIDTH, HEIGHT);
-	draw_content(*test_image, test);
-	mlx_image_to_window(window, test_image, 0, 0);
-	mlx_loop(window);
-	mlx_terminate(window);
 	return (0);
-}
-
-void	draw_content(mlx_image_t image, char **content)
-{
-	int	i;
-	int	j;
-	int	line_offset;
-
-	i = 0;
-	j = 0;
-	line_offset = 0;
-	while (content[i] != NULL)
-	{
-		while (content[i][j] != '\0')
-		{
-			ft_memset(image.pixels + 5 * j + line_offset, 255, 5 * 5 * sizeof(int32_t));
-			j++;
-		}
-		line_offset += WIDTH;
-		i++;
-	}
 }
 
 int	count_newlines(int fd)
