f03fd7a9a45941bad67b3739a999ca175bfc3f86
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-01-11 08:44:32 +0100

    add window initialisation

--- src/fdf.c
+++ src/fdf.c
@@ -6,27 +6,41 @@
 /*   By: pschwarz <pschwarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 15:08:16 by pschwarz          #+#    #+#             */
-/*   Updated: 2023/01/10 17:54:45 by pschwarz         ###   ########.fr       */
+/*   Updated: 2023/01/11 08:42:34 by pschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
 
+mlx_t	*init_mlx(void);
+
 int	main(int argc, char **argv)
 {
 	int				i;
 	t_coordinates	**coordinate_map;
+	mlx_t			*mlx;
 
 	if (argc != 2)
 		return (0);
 	i = 0;
 	coordinate_map = parse_map(argv[argc - 1]);
+	mlx = init_mlx();
 	while (coordinate_map[i] != NULL)
 	{
 		ft_printf("x: %d, y: %d, z: %d\n", coordinate_map[i]->x,
 			coordinate_map[i]->y, coordinate_map[i]->z);
 		i++;
 	}
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
 	free(coordinate_map);
 	return (0);
+}
+
+mlx_t	*init_mlx(void)
+{
+	mlx_t	*mlx;
+
+	mlx = mlx_init(WIDTH, HEIGHT, "fdf", false);
+	return (mlx);
 }
